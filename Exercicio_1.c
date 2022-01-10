fc4ff3230d5af6372cd1cffde6de3c32df162f2b
LuizHenriqueFN <92808528+LuizHenriqueFN@users.noreply.github.com>
2022-01-10 19:28:26 -0300

    Add files via upload

---
+++ Exercicio_1.c
@@ -0,0 +1,284 @@
+/**************************COMPONENTES DO GRUPO***************************
+        LUIZ HENRIQUE FERREIRA NEVES E DANILO ARAUJO DOMINGUES SILVA
+**************************************************************************/
+
+/*
+Exercício 1.
+    Considere como universo de valores o conjunto de números inteiros 0 ≤ x ≤ 50. Considere, ainda, dois
+    conjuntos A e B, contendo 10 elementos aleatório cada, dentro do universo de valores possíveis.
+    Devem ser criados algoritmos proceder as seguintes operações:
+        • A ∪ B
+        • A ∩ B
+        • A − B
+        • B − A
+        • ¬A
+    O sistema deverá imprimir os conjuntos originais, A e B, e, em seguida, imprimir o resultado das oper-
+    ações. Podem ser criadas funções para implementação de cada uma das operações, evitando repetição
+    de trabalho.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define TAM 10
+#define LIM 51
+
+
+//Variáveis globais:
+int A[TAM], B[TAM];
+
+//*************************** Funções para gerar e organizar valores ***************************//
+
+//Função para verificar se existem números repetidos no conjunto:
+int Repetido(int *Vet, int i, int num){
+    for(int j = 0; j < i; j++){
+        if(Vet[j] == num){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+//Função para preencher o conjunto para que os valores não se repitam:
+void GeraAleatorio(int *Vet){
+    int num;
+    srand(time(NULL));
+
+    for(int i = 0; i < TAM; i++){
+        num = rand() % LIM;
+
+        while(Repetido(Vet, i, num)){
+            num = rand() % LIM;
+        }
+
+        Vet[i] = num;
+    }
+}
+
+//Função para ordenar o conjunto de forma crescente:
+void Ordena_Vet(int *Vet, int tam){
+    int aux;
+    for(int j = 0; j < tam; j++){
+        for(int i = 0; i < tam; i++){
+            if(Vet[i] > Vet[i+1] && i+1 != tam){
+                aux = Vet[i];
+                Vet[i] = Vet[i+1];
+                Vet[i+1] = aux;
+            }
+        } 
+    }
+}
+//**********************************************************************************************//
+
+
+//***************************** Funções para cálculos *****************************//
+
+//Função para unir os dois conjuntos sem repetir os valores:
+void Uniao(int *Resultado){
+    int aux, posicao = TAM;
+
+    for(int i = 0; i < TAM; i++){
+        Resultado[i] = A[i];
+    }
+    for(int i = 0; i < TAM; i++){
+        aux = 0;
+        for(int j = 0; j < TAM; j++){
+            if(B[i] == A[j]){
+                aux++;
+            }
+        }
+        if(aux == 0){
+            Resultado[posicao++] = B[i];
+        }
+    }
+    //Chamando a função que ordena o conjunto:
+    Ordena_Vet(Resultado, posicao-1);
+
+}
+
+//Função que verifica quais os números aparecem nos dois conjuntos:
+void Intersecao(int *Resultado){
+    int aux = 0;
+    for(int i = 0; i < TAM; i++){
+        for(int j = 0; j < TAM; j++){
+            if(A[i] == B[j]){
+                Resultado[aux++] = A[i];
+            }
+        }
+    }
+    //Chamando a função que ordena o conjunto:
+    Ordena_Vet(Resultado, aux-1);
+}
+
+//Função que calcula a diferença entre os conjuntos:
+void Diferenca(int *Resultado, int opcao){
+    int aux, Posicao = 0;
+
+    //Opção: 1- A-B; 2- B-A.
+    if(opcao == 1){
+        for(int i = 0; i < TAM; i++){
+            aux = 0;
+            for(int j = 0; j < TAM; j++){
+                if(A[i] == B[j]){
+                    aux++;
+                }
+            }
+            if(aux == 0){
+                Resultado[Posicao++] = A[i];
+            }
+        }
+
+    }else{
+        for(int i = 0; i < TAM; i++){
+            aux = 0;
+            for(int j = 0; j < TAM; j++){
+                if(B[i] == A[j]){
+                    aux++;
+                }
+            }
+            if(aux == 0){
+                Resultado[Posicao++] = B[i];
+            }
+        }
+    }
+    //Chamando a função que ordena o conjunto:
+    Ordena_Vet(Resultado, Posicao-1);
+}
+
+//Função que calcula quais os valores que não estão presentes no conjunto:
+void Complemento(int *Resultado){
+    int Saida = 1, num;
+    srand(time(NULL));
+    for(int i = 0; i < 41; i++){
+        num = rand() % LIM;
+        Saida = 1;
+        while(Saida){
+            Saida = Repetido(Resultado, i, num);
+            for(int j = 0; j < TAM; j++){
+                if(A[j] == num){
+                    Saida++;
+                }
+            }
+            if(Saida == 1){
+                num = rand() % LIM;
+            }
+            printf("Saida:%d Num:%d\n", Saida, num);
+        }
+        Resultado[i] = num;
+        
+    }
+    //Chamando a função que ordena o conjunto:
+    Ordena_Vet(Resultado, 41);
+}
+//*********************************************************************************//
+
+//Função MENU para verificar a escolha do usuário:
+void MENU(int Operacao, int Tamanho){
+    int Resultado[Tamanho];// !- Declarando uma váriavel utilizando uma variável para definir o tamanho.
+
+    //Limpando Vetor:
+    for(int i = 0; i < Tamanho; i++){
+        Resultado[i] = 0;
+    }
+
+    switch(Operacao){
+        case 1:
+            Uniao(Resultado);
+            break;
+        case 2:
+            Intersecao(Resultado);
+            break;
+        case 3:
+            Diferenca(Resultado, 1);
+            break;
+        case 4:
+            Diferenca(Resultado, 2);
+            break;
+        case 5:
+            Complemento(Resultado);
+            break;
+        case 6:
+            //Função para gerar números aleatórios diferentes:
+            GeraAleatorio(A);
+            sleep(1);//Pausa para não repetir os valores.
+            GeraAleatorio(B);
+
+            //Função para ordenar:
+            Ordena_Vet(A, TAM);
+            Ordena_Vet(B, TAM);
+            return;
+        default:
+            printf("\nA operação escolhida não existe. Tente novamente:\n");
+            sleep(2);
+            system("clear");
+            return;
+    }
+    
+    //Exibindo Resultado:
+    printf("\nResultado = %d ", Resultado[0]);
+    for(int i = 1; i < Tamanho; i++){
+        if(Resultado[i] != 0){
+            printf("%d ", Resultado[i]);
+        }
+    }
+    printf("\n");
+
+}
+
+int main(){
+    system("clear");
+    int operacao, Tamanho;
+
+    //Função para gerar números aleatórios diferentes:
+    GeraAleatorio(A);
+    sleep(1);//Pausa para não repetir os valores.
+    GeraAleatorio(B);
+
+    //Função para ordenar:
+    Ordena_Vet(A, TAM);
+    Ordena_Vet(B, TAM);
+
+    //Menu de operações:
+    do{
+        //Exibir conjuntos:
+        printf("\tA = ");
+        for(int i = 0; i < TAM; i++){
+            printf("%d ", A[i]);
+        }
+        printf("\n\tB = ");
+        for(int i = 0; i < TAM; i++){
+            printf("%d ", B[i]);
+        }
+
+        //Menu:
+        printf("\n************************** Menu **************************\n");
+        printf("1 - A U B;\n2 - A ∩ B;\n3 - A - B;\n4 - B - A;\n5 - ¬A;\n6 - Gerar novos números;\n0 - Sair;\nEscolha: ");
+        scanf("%d", &operacao);
+
+        //Definindo tamanho do vetor resultado:
+        switch(operacao){
+            case 1:
+                Tamanho = 20;
+                break;
+            case 2:
+            case 3:
+            case 4:
+                Tamanho = 10;
+                break;
+            case 5:
+                Tamanho = 41;
+                break;
+        }
+
+        //Chamar MENU ou sair:
+        if(operacao != 0){
+            MENU(operacao, Tamanho);
+        }else{
+            system("clear");
+            printf("\nEncerrando...");
+        }
+
+    }while(operacao != 0);
+
+    return 0;
+}
---
+++ Exercicio_3.c
@@ -0,0 +1,212 @@
+/**************************COMPONENTES DO GRUPO***************************
+        LUIZ HENRIQUE FERREIRA NEVES E DANILO ARAUJO DOMINGUES SILVA
+**************************************************************************/
+/*
+Exercício 3.
+    Considere como universo de valores o conjunto de números inteiros 0 ≤ x ≤ 4. Gere uma relação
+    aleatória R contendo 10 elementos ordenados (ex.: R = {(0, 0),(0, 4),(1, 1),(1, 2),(2, 0), ...,(4, 1)}).
+    A relação pode ser representada por uma matriz 10 × 2, onde cada linha representa uma tupla.
+    Analise se a relação gerada aleatoriamente é:
+        • Reflexiva;
+        • Simétrica;
+        • Antissimétrica;
+        • Transitiva.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define Linha 10
+#define Coluna 2
+#define Lim 5
+
+//Função criada para exibição da matriz:
+void ExibirMat(int Mat[][Coluna]){
+    printf("\n");
+    for(int i = 0; i < Linha; i++){
+        for(int j = 0; j < Coluna; j++){
+            printf("%d ", Mat[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+//Função para analisar se a matriz criada, que possui uma "divisão" por tuplas,
+//possui ou não a caracteristica de ser reflexiva
+void Reflexivo(int Mat[][Coluna], int *Boolean){
+    srand(time(NULL));
+    int Refle[Lim] = {}, Tam = 0, aux = 0;
+
+    //Tam = Variável que contém quantos números apareceram.
+    for(int i = 0; i < Lim; i++){
+        Tam += Boolean[i];
+    }
+
+    //Verificando se os pares são reflexivos:
+    for(int i = 0; i < Lim; i++){
+        for(int j = 0; j < Linha; j++){
+            //Condição para verificar se o par ordenado está presente na matriz:
+            if(Mat[j][0] == i && Mat[j][1] == i && Boolean[i] == 1){
+                Refle[i] = 1;
+            }
+        }
+    }
+
+    //aux = Variavel que contem quantos pares apareceram.
+    for(int i = 0; i < Lim; i++){
+        aux += Refle[i];
+    }
+
+    //Verificando se o a quantidade de números é igual a quantidade de pares de cada número presente:
+    if(aux == Tam){
+        printf("\tReflexivo: Sim.");
+    }else{
+        printf("\tReflexivo: Não.");
+    }
+    return 0;
+}
+
+//Função que verifica se a matriz criada, que possui uma "divisão" por tuplas,
+//possui ou não a caracteristica de ser simetrica
+void Simetrico(int Mat[][Coluna]){
+    int x, y, aux = 0;
+
+    for(int i = 0; i < Linha; i++){
+        x = Mat[i][0]; //Os valores das casas são armazenados nas variáveis.
+        y = Mat[i][1];
+
+        for(int j = 0; j < Linha; j++){
+            if(Mat[j][0] == y && Mat[j][1] == x){//É então verificado se, quando invertemos estes valores de posicão,
+                aux++;                           //há algum par que seja igual ao que foi invertido, e caso haja, a função auxiliar terá a função de contador.
+                break;                           
+            }
+        }
+    }
+
+    if(aux == 10){//Caso a soma total seja 10, ou seja todos os pares correspondem a um outro inverso, logo, é simétrico.
+        printf("\n\tSimétrico: Sim.");
+    }else{
+        printf("\n\tSimétrico: Não.");
+    }
+}
+
+//Função que analisa se a matriz gerada é antissimetrica:
+void Antissimetrico(int Mat[][Coluna]){
+    int x, y, aux = 0;
+
+    for(int i = 0; i < Linha; i++){
+        x = Mat[i][0]; //Os valores das casas são armazenados nas variáveis. 
+        y = Mat[i][1];
+        if(x == y){
+            continue;
+        }
+
+        for(int j = 0; j < Linha; j++){
+            if(Mat[j][0] == y && Mat[j][1] == x){//É então verificado se, quando invertemos estes valores de posição,
+                aux = 1;                         //há algum par que seja igual ao que foi invertido
+            }                                    //caso haja, então não é antissimetrico.
+        }
+    }
+
+    if(aux == 0){
+        printf("\n\tAntissimétrico: Sim.");
+    }else{
+        printf("\n\tAntissimétrico: Não.");
+    }
+}
+
+//Função que verifica se a matriz gerada possui a propriedade transitiva:
+void Transitivo(int Mat[][Coluna]){
+    int x, y, z, Saida = 0;
+
+    for(int i = 0; i < Linha; i++){
+        x = Mat[i][0];//Os números das casas de cada linha são armazenados em variáveis cada vez que o ciclo 'for' é executado.
+        y = Mat[i][1];
+
+        for(int j = 0; j < Linha; j++){
+            if(Mat[j][0] == y){//Caso o primeiro número de um par de uma linha qualquer seja igual a y,
+                z = Mat[j][1];//então o segundo número deste par desta linha qualquer é armazenado na variavel z.
+                for(int k = 0; k < Linha; k++){
+                    if(Mat[k][0] == x && Mat[k][1] == z){//Caso haja um par de números em uma linha qualquer que possua x como o primeiro número e z como o segundo,
+                        break;                           //temos então uma relacão transitiva.
+                    }
+                    if(k == Linha-1){//Condição que define para o algoritmo até onde ele vai ser executado.
+                        Saida = 1;
+                        break;
+                    }
+                    
+                }
+            }
+            if(Saida == 1){
+                break;
+            }
+
+        }
+       
+        if(Saida == 1){
+            break;
+        }
+        
+    }
+    if(Saida == 0){//Se a saida for 0, o código foi executado e não foi achado nenhum erro, então a matriz é transitiva.
+        printf("\n\tTransitivo: Sim.");
+    }else{
+        printf("\n\tTransitivo: Não.");
+    }
+}
+
+int main(){//Função principal:
+    srand(time(NULL));
+    int Boolean[Lim] = {}, num, opcao;
+    int Mat[Linha][Coluna] = {};//Criando matriz vazia para depois ser preenchida.
+
+    for(int i = 0; i < Linha; i++){
+        for(int j = 0; j < Coluna; j++){
+            num = rand() % Lim;
+
+            //Verificando os valores que existem na matriz:
+            Boolean[num] = 1;
+            Mat[i][j] = num;
+        }
+    }
+
+    ExibirMat(Mat);
+
+    //Chamando Funções para verificar se a matriz possui as propriedades reflexiva, simétrica, antissimétrica e transitiva, respectivamente.
+    Reflexivo(Mat, Boolean);
+    Simetrico(Mat);
+    Antissimetrico(Mat);
+    Transitivo(Mat);
+
+    do{//Menu do programa principal:
+        printf("\n************************** Menu **************************\n");
+        printf("0- Sair;\n1- Gerar novos números;\nOpção: ");//Exibindo na tela as opções.
+        scanf("%d", &opcao);//Recebendo as opções do usuário.
+
+        if(opcao == 1){//Se o usuário tiver escolhido a posição 1, então uma nova matriz será gerada e analisada novamente.
+            for(int i = 0; i < Linha; i++){
+                for(int j = 0; j < Coluna; j++){
+                    num = rand() % 5;
+
+                    //Verificando os valores que existem  na matriz:
+                    Boolean[num] = 1;
+                    Mat[i][j] = num;
+                }
+            }
+
+            ExibirMat(Mat);
+
+            //Chamando Funções para verificar se a matriz possui as propriedades reflexiva, simétrica, antissimétrica e transitiva, respectivamente.
+            Reflexivo(Mat, Boolean);
+            Simetrico(Mat);
+            Antissimetrico(Mat);
+            Transitivo(Mat);
+        }else{
+            system("clear");//Limpando terminal.
+            printf("Encerrando...");
+        }
+    }while(opcao);
+
+    return 0;
+}
